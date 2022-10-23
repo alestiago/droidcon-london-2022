e5afd5dd58890fb18f43e82fbbd80a216a040236
alestiago <dev@alestiago.com>
2022-10-23 17:03:53 +0100

    chore: flutter create forge2d_picker

--- examples/forge2_picker/android/app/src/main/kotlin/com/example/forge2_picker/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.forge2_picker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ examples/forge2d_picker/android/app/src/main/kotlin/com/example/forge2d_picker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.forge2d_picker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
