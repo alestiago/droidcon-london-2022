f724a358abaa6f5c63817a71d7fbd9cf1c4c444b
alestiago <dev@alestiago.com>
2022-10-24 11:03:47 +0100

    feat: included FavouritesBloc

--- examples/forge2d_picker/lib/game/music_genre_picker.dart
+++ examples/forge2d_picker/lib/game/music_genre_picker.dart
@@ -2,15 +2,20 @@ import 'dart:math' as math;
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flame_forge2d/flame_forge2d.dart';
+import 'package:flame_bloc/flame_bloc.dart';
 import 'package:flutter/painting.dart';
+import 'package:forge2d_picker/bloc/favourites_bloc.dart';
 import 'package:forge2d_picker/game/game.dart';
 
 class MusicGenrePicker extends Forge2DGame with HasTappables {
-  MusicGenrePicker()
-      : super(
+  MusicGenrePicker({
+    required this.favouritesBloc,
+  }) : super(
           gravity: Vector2.zero(),
           zoom: 1,
         );
+
+  final FavouritesBloc favouritesBloc;
 
   final _random = math.Random();
 
@@ -39,6 +44,12 @@ class MusicGenrePicker extends Forge2DGame with HasTappables {
         ),
       );
     }
-    await addAll(children);
+
+    await add(
+      FlameBlocProvider<FavouritesBloc, FavouritesState>.value(
+        value: favouritesBloc,
+        children: children,
+      ),
+    );
   }
 }
--- examples/forge2d_picker/lib/main.dart
+++ examples/forge2d_picker/lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flame/game.dart';
-import 'package:flame_forge2d/flame_forge2d.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:forge2d_picker/bloc/favourites_bloc.dart';
 import 'package:forge2d_picker/game/game.dart';
 
 void main() {
-  runApp(GameWidget(game: MusicGenrePicker()));
+  runApp(
+    BlocProvider<FavouritesBloc>(
+      create: (context) => FavouritesBloc(),
+      child: Builder(
+        builder: (context) {
+          return GameWidget(
+            game: MusicGenrePicker(
+              favouritesBloc: context.read<FavouritesBloc>(),
+            ),
+          );
+        },
+      ),
+    ),
+  );
 }
